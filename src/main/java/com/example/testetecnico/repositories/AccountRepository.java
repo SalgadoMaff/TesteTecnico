d379e0da1be5acad67eb24800fdb1cb090b32fe2
Felipe <lipesmaffini@gmail.com>
2024-05-24 23:02:30 -0400

    created find by email

--- src/main/java/com/example/testetecnico/repositories/AccountRepository.java
+++ src/main/java/com/example/testetecnico/repositories/AccountRepository.java
@@ -2,7 +2,14 @@ package com.example.testetecnico.repositories;
 
 import com.example.testetecnico.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    @Query("SELECT a FROM Account a WHERE a.email = :email")
+    Optional<Account> findByEmail(@Param("email") String email);
 }
 
