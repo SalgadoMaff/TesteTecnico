0e506d1048a5a9466e8468b8d1b26bf7b7366436
Felipe <lipesmaffini@gmail.com>
2024-05-27 18:23:34 -0400

    remove unnecessary logic and implements the relationship on Profile

--- src/main/java/com/example/testetecnico/entities/Movie.java
+++ src/main/java/com/example/testetecnico/entities/Movie.java
@@ -2,15 +2,11 @@ package com.example.testetecnico.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "movie")
@@ -24,9 +20,4 @@ public class Movie {
     private String title;
     private String overview;
 
-    @ManyToMany(mappedBy = "planToWatch")
-    private Set<Profile> profilesPlanToWatch = new HashSet<>();
-
-    @ManyToMany(mappedBy = "watched")
-    private Set<Profile> profilesWatched = new HashSet<>();
 }
--- src/main/java/com/example/testetecnico/entities/Profile.java
+++ src/main/java/com/example/testetecnico/entities/Profile.java
@@ -1,15 +1,19 @@
 package com.example.testetecnico.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "profile")
-@Getter
-@Setter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,12 +21,18 @@ public class Profile {
     private String name;
 
     @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
     private Account account;
 
-//    @OneToMany(mappedBy = "profile", cascade = CascadeType.ALL)
-//    private List<Movie> planToWatch;
-//
-//    @OneToMany(mappedBy = "profile", cascade = CascadeType.ALL)
-//    private List<Movie> watchedList;
+    @ManyToMany
+    @JoinTable(name = "profile_watched",
+            joinColumns = @JoinColumn(name = "profile_id"),
+            inverseJoinColumns = @JoinColumn(name = "movie_id"))
+    private Set<Movie> watched;
 
+    @ManyToMany
+    @JoinTable(name = "profile_plan_to_watch",
+            joinColumns = @JoinColumn(name = "profile_id"),
+            inverseJoinColumns = @JoinColumn(name = "movie_id"))
+    private Set<Movie> planToWatch;
 }
