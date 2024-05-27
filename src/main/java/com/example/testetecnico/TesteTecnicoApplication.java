a1732187333a45e2f00ea128e1e088f1e45270ee
Felipe <lipesmaffini@gmail.com>
2024-05-27 15:43:01 -0400

    moving bean security to main()

--- src/main/java/com/example/testetecnico/TesteTecnicoApplication.java
+++ src/main/java/com/example/testetecnico/TesteTecnicoApplication.java
@@ -2,12 +2,15 @@ package com.example.testetecnico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@EnableWebSecurity
+@EnableMethodSecurity
+@SpringBootApplication(scanBasePackages = {"com.example.testetecnico"})
 public class TesteTecnicoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TesteTecnicoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TesteTecnicoApplication.class, args);
+    }
 
 }
