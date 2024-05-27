9ced1f92c4da39508d38caba39404b4c34635edc
Felipe <lipesmaffini@gmail.com>
2024-05-27 15:39:44 -0400

    ProfileController refactored

--- src/main/java/com/example/testetecnico/controller/ProfileController.java
+++ src/main/java/com/example/testetecnico/controller/ProfileController.java
@@ -2,17 +2,24 @@ package com.example.testetecnico.controller;
 
 import com.example.testetecnico.entities.Profile;
 import com.example.testetecnico.services.AccountService;
+import com.example.testetecnico.services.CustomUserDetails;
 import com.example.testetecnico.services.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
+@RequestMapping("/profiles")
 public class ProfileController {
     @Autowired
     private ProfileService profileService;
@@ -20,15 +27,16 @@ public class ProfileController {
     private AccountService accountService;
 
 
-    @GetMapping("/account/{id}/profiles/")
-    public String getProfiles(@PathVariable Long id, Model model) {
+    @GetMapping
+    public String getProfiles(Model model) {
+        Long id = getCredentialId();
         try {
             var account = accountService.findById(id);
-            if(account.isPresent()){
-                model.addAttribute("account",account.get());
+            if (account.isPresent()) {
+                model.addAttribute("account", account.get());
                 return "profiles";
 
-            }else{
+            } else {
                 throw new UsernameNotFoundException("not found");
             }
         } catch (AccessDeniedException e) {
@@ -40,21 +48,31 @@ public class ProfileController {
 
     }
 
-    @GetMapping("/account/{id}/register-profile")
-    public String showRegisterProfileForm(@PathVariable Long id, Model model) {
+
+    @GetMapping("/register-profile")
+    public String showRegisterProfileForm(Model model) {
+        Long id = getCredentialId();
         model.addAttribute("accountId", id);
         return "register-profile";
     }
 
-    @PostMapping("/account/{id}/register-profile")
-    public String addProfile(@PathVariable Long id, @RequestParam String profileName) {
-        var account=accountService.findById(id);
+    @PostMapping("/register-profile")
+    public String addProfile(@RequestParam String profileName) {
+        Long id = getCredentialId();
+        var account = accountService.findById(id);
         Profile profile = new Profile();
         profile.setName(profileName);
-        profileService.addProfile(account.get(),profile);
+        profileService.addProfile(account.get(), profile);
 
-        return "redirect:/account/" + id + "/profiles/";
+        return "redirect:/profiles";
 
+    }
+
+    private static Long getCredentialId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        Long id = userDetails.getId();
+        return id;
     }
 
 }
