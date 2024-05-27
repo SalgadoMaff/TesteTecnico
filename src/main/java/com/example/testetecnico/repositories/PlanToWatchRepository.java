8be7a2662c4c80fca0cdfb689b384a0cad7b6d4f
Felipe <lipesmaffini@gmail.com>
2024-05-27 18:24:44 -0400

    Added Querys to the repository

--- src/main/java/com/example/testetecnico/repositories/PlanToWatchRepository.java
+++ src/main/java/com/example/testetecnico/repositories/PlanToWatchRepository.java
@@ -4,10 +4,14 @@ import com.example.testetecnico.entities.PlanToWatch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PlanToWatchRepository extends JpaRepository<PlanToWatch, Long> {
 
     @Query("SELECT p FROM PlanToWatch p WHERE p.movieId = :movieId AND p.profileId = :profileId")
     Optional<PlanToWatch> findByProfileIdAndMovieId(Long profileId, Long movieId);
+
+    @Query("SELECT ptw FROM PlanToWatch ptw WHERE ptw.profileId = :profileId")
+    Optional<List<PlanToWatch>> getAllById(Long profileId);
 }
--- src/main/java/com/example/testetecnico/repositories/WatchedRepository.java
+++ src/main/java/com/example/testetecnico/repositories/WatchedRepository.java
@@ -4,10 +4,15 @@ import com.example.testetecnico.entities.Watched;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WatchedRepository extends JpaRepository<Watched, Long> {
 
     @Query("SELECT w FROM Watched w WHERE w.movieId = :movieId AND w.profileId = :profileId")
     Optional<Watched> findByProfileIdAndMovieId(Long profileId, Long movieId);
+
+    @Query("SELECT w FROM Watched w WHERE w.profileId = :profileId")
+    Optional<List<Watched>> getAllById(Long profileId);
+
 }
